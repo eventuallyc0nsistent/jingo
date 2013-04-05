00b1a58b1faa025a5db9a27fd375ec1e823cf286
Kiran Koduru <kk@energysolforum.com>
2013-04-05 19:55:13 -0400

    new intro page

--- header.php
+++ header.php
@@ -1,5 +1,9 @@
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="include/css/bootstrap.css">
+	<script type="text/javascript" src="include/js/bootstrap.js"></script>
 	<title>Jingo</title>
+
 </head>
 <body>
+	<div class="container">
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
-<?php 
-
-
-
-
-
-
-
-
-?>
+<?php 	require_once('header.php') ; ?>
+<div class="hero-unit">
+ <h1>Whats our story ?</h1>
+ 
+  <p>Suppose you discover a nice restaurant (by finding it on the web or by walking by it) and you think your friends might like this place. Or you find a nice pair of shoes in a shop window. It would be useful if you could post a note about this place, such that your friends (or maybe everybody) receive the note if they come within 300 yards of this place. Or maybe only if they come within 300 yards during lunch time.</p>
+  <p>
+    <a class="btn btn-primary btn-large">
+      Get Started
+    </a>
+  </p>
+</div>
+<?php require_once('footer.php') ; ?>
