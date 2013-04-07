afb35d0fca15953892a7be259cebdae7aa39d33f
Kiran Koduru <kk@energysolforum.com>
2013-04-07 00:27:50 -0400

    href change

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  
   <p>Suppose you discover a nice restaurant (by finding it on the web or by walking by it) and you think your friends might like this place. Or you find a nice pair of shoes in a shop window. It would be useful if you could post a note about this place, such that your friends (or maybe everybody) receive the note if they come within 300 yards of this place. Or maybe only if they come within 300 yards during lunch time.</p>
   <p>
-    <a href="login.php?code=GP14TDE3VMVSDKUKHD2DXL1JM0FCTEQDESEURQMY1KRKNPD5">
+    <a href="https://foursquare.com/oauth2/authenticate?client_id=5LENUVBOHJTM5EHQT11GZZYZVVWSRLIIL1WGSRBRF0RMXN2I&response_type=code&redirect_uri=https://jingo.herokuapp.com/login.php">
 	  <img alt="Foursquare" src="https://playfoursquare.s3.amazonaws.com/press/logo/connect-blue.png">
 	 </a>
   </p>
