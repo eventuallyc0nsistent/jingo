01ff2bf486654f1a47810a4db9ab0dcaf1bf50be
Kiran Koduru <kk@energysolforum.com>
2013-04-07 00:11:01 -0400

    added jQuery

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="include/css/bootstrap.css">
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script type="text/javascript" src="include/js/bootstrap.js"></script>
 	<title>Jingo</title>
 
