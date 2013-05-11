6f670e1a541ae22e20359822f2c54bbb10b1fac2
Kiran Koduru <kiranrkoduru@gmail.com>
2013-05-10 23:04:46 -0400

    ----footer not being called on my site argh

--- view/userhome.php
+++ view/userhome.php
@@ -1,62 +1,21 @@
-<<<<<<< HEAD
 <?php  require_once('header.php'); ?>
-=======
-<?php  
-require_once('header.php'); 
-
-$username = $_SESSION['username'];
-
-$query = "SELECT * FROM USER WHERE username = '".$username."'";
-$result = $mysqli->query($query);
-
-// fetch associative array of the result
-$row = $result->fetch_array();
-
-$firstname = $row['firstname'];
-$lastname = $row['lastname'];
-
-
-?>
-
-
->>>>>>> b479afad5bf27fd893585cfc9f94990967d74e2e
 
 <div class="span3 well">
 	<div class="row">
 		<div class="span1"><a href="http://critterapp.pagodabox.com/others/admin" class="thumbnail"><img src="http://critterapp.pagodabox.com/img/user.jpg" alt=""></a></div>
 		<div class="span2">
-<<<<<<< HEAD
 			<p>admin</p>
           	<p><strong>First Last Name</strong></p>
 		</div>
 		<br/>
 		<div class="span3">
 			<span class=" badge badge-warning">8 messages</span> <span class=" badge badge-info">15 followers</span>
-=======
-			<p><a href="#">@<?php echo $username ; ?></a></p>
-          	<p><strong><?php echo $firstname.' '.$lastname ?></strong></p>
-		</div>
-		<div class="span3 mt10">
-			<span class=" badge badge-warning">8 notes</span> <span class=" badge badge-info">15 followers</span>
-		</div>
-		<div class="span4 mt10">
-		    <form accept-charset="UTF-8" action="post_note.php" method="POST">
-		        <textarea class="span3" id="new_message" name="new_message"
-		        placeholder="Type in your message" rows="3"></textarea>
-		        <h6>320 characters remaining</h6>
-		        <button class="btn btn-info" type="submit">Post Note</button>
-		    </form>
->>>>>>> b479afad5bf27fd893585cfc9f94990967d74e2e
 		</div>
 	</div>
 </div>
 
 <div class="span6 well">
 	<h2>Notes</h2>
-<<<<<<< HEAD
-=======
-	<hr>
->>>>>>> b479afad5bf27fd893585cfc9f94990967d74e2e
 	<div class="row">
 		<div class="span1"><a href="http://critterapp.pagodabox.com/others/admin" class="thumbnail"><img src="http://critterapp.pagodabox.com/img/user.jpg" alt=""></a></div>
 		<div class="span5">
